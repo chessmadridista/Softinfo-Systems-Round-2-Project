9fa180eb5e48a5f0a35bea7da0a3e7dc46196c81
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-30 00:14:59 +0530

    Attach app bar to the app.

---
+++ app/src/components/NavigationHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app-bar app>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>
+            Home
+        </v-toolbar-title>
+    </v-app-bar>
+</template>
+<script>
+export default {
+    name: "NavigationHeader",
+};
+</script>
