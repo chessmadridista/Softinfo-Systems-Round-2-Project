5587841eaee8879a31513ef1e310c6027f08ee23
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-30 16:30:12 +0530

    Create blueprint for 'Home' component.

---
+++ app/src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-container>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+        <v-card>
+            Home
+        </v-card>
+    </v-container>
+</template>
+<script>
+export default {
+    name: "Home",
+}
+</script>
