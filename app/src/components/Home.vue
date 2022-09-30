43a1efd32dcee0e79eb6cf5eda723b26ce401633
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-30 18:13:55 +0530

    Fetch table data when the 'Home' component gets mounted.

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -47,5 +47,50 @@
 <script>
 export default {
     name: "Home",
+    data() {
+        return {
+            tableHeaders: [
+                "API",
+                "Description",
+                "Auth",
+                "HTTPS",
+                "Cors",
+                "Link",
+                "Category",
+            ],
+            tableItems: [],
+        };
+    },
+    methods: {
+        async getData() {
+            const URL = "https://api.publicapis.org/entries";
+
+            return fetch(URL)
+                .then((response) => {
+                    console.log(response);
+
+                    return response.json();
+                })
+                .then((data) => {
+                    return data.entries;
+                });
+        },
+        setData(data) {
+            this.tableItems = data;
+
+            return true;
+        },
+        initialiseComponent() {
+            this.getData()
+            .then((data) => {
+                this.setData(data);
+            });
+
+            return true;
+        },
+    },
+    mounted() {
+        this.initialiseComponent();
+    },
 }
 </script>
