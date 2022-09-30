16baf90625b003d4e319ecd9a25b9d2c2280a979
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-30 16:36:10 +0530

    Add routes for the created components.

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue';
+import Explore from '../components/Explore.vue';
+import Settings from '../components/Settings.vue';
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home,
+  },
+  {
+    path: '/explore',
+    name: 'explore',
+    component: Explore,
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: Settings,
   },
 ];
 
